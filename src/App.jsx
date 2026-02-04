1965885b4956c01569ba891a3ef44b78699b27c1
Hector <52350216+Higna@users.noreply.github.com>
2026-02-04 22:31:12 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router";
 import { HomePage } from "./pages/Index";
+import { PortfolioPage } from "./pages/Content";
 import { NavBar, Footer } from "./components/Bar";
 import { useTheme } from "./context/ThemeContext";
 import { ThemeProvider } from "./context/ThemeContext";
@@ -19,6 +20,7 @@ const AppLayout = () => {
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/portfolio" element={<PortfolioPage />}/> 
           </Routes>
         </main>
         <Footer />
---
+++ src/pages/Content/PortfolioPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      This is the portfolio page
+    </div>
+  )
+}
+
+export default PortfolioPage
