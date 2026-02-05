d68ecd3f4cd7e511254f18f1fedb3a000edb5c86
Hector <52350216+Higna@users.noreply.github.com>
2026-02-05 16:28:19 +0100

    Update

--- src/components/Bar/Footer.jsx
+++ src/components/Bar/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
           <div className="flex gap-4">
             <Link to={"/privacy"}>Privacy</Link>
             <Link to={"/terms"}>Terms</Link>
-            <Link to={"/about"}>Contact</Link>
+            <Link to={"/about#contact"}>Contact</Link>
           </div>
         </div>
       </div>
